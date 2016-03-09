aa03da4265747eb299e194771e92c48f643d9cce
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-09 18:04:19 +0200

    generate specific hash based on a rule

--- hashdir.go
+++ hashdir.go
@@ -2,11 +2,34 @@ package hashdir
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"errors"
 	"fmt"
+	"hash"
 	"io"
 	"os"
 	"path/filepath"
 )
+
+const (
+	SHA1   = "sha1"
+	SHA256 = "sha256"
+	MD5    = "md5"
+)
+
+func CreateHash(name *string) (hash.Hash, error) {
+	if *name == SHA1 {
+		return sha1.New(), nil
+	} else if *name == SHA256 {
+		return sha256.New(), nil
+	} else if *name == MD5 {
+		return md5.New(), nil
+	} else {
+		err := errors.New("error")
+		return nil, err
+	}
+}
 
 // Create an md5 hash with local path
 func Create(path string) (string, error) {
