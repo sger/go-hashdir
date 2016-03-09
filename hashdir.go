ca7050587c31754836ed0e0620f870771c951a7f
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-09 17:49:02 +0200

    first commit

---
+++ hashdir.go
@@ -0,0 +1,28 @@
+package hashdir
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func hashDir(path string) (string, error) {
+
+	md5Hash := md5.New()
+
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		io.WriteString(md5Hash, path)
+		return nil
+	})
+
+	if err != nil {
+		return "", nil
+	}
+
+	return fmt.Sprintf("%x", md5Hash.Sum(nil)), nil
+}
