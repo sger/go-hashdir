606b676c3177796c2e560019527244b537e0812a
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-09 20:02:05 +0200

    updates

--- hashdir_test.go
+++ hashdir_test.go
@@ -22,5 +22,13 @@ func TestHashDir(t *testing.T) {
 	hash4, err := hashdir.Create("test/directory1", "sha1")
 	require.NoError(t, err)
 
-	require.Equal(t, hash3, hash4, "hash3 and hash 4 are equals")
+	require.Equal(t, hash3, hash4, "hash3 and hash4 are equals")
+
+	hash5, err := hashdir.Create("test/directory2", "sha1")
+	require.NoError(t, err)
+
+	hash6, err := hashdir.Create("test/directory2", "sha1")
+	require.NoError(t, err)
+
+	require.Equal(t, hash5, hash6, "hash5 and hash6 are equals")
 }
